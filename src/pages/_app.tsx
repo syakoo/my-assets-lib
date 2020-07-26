7d19a160d8c05c738a7405b9a67c22d02c229660
syakoo <nichijou4009@gmail.com>
2020-07-26 22:14:38 +0900

    Update: add head and icon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,23 @@
 import { AppProps } from 'next/app'
+import Head from 'next/head'
 
 // ____________________________________________________________________________
 //
 const App = ({ Component, pageProps }: AppProps): JSX.Element => {
   return (
     <>
+      <Head>
+        <title>My Assets Lib</title>
+        <link rel="shortcut icon" href="my-assets-lib.png" />
+        <meta property="description" content="SyakooのAssetsの図書館" />
+        <meta property="og:title" content="My Assets Lib" />
+        <meta
+          property="og:image"
+          content="https://syakoo.github.io/my-assets-lib/my-assets-lib.png"
+        />
+        <meta property="twitter:card" content="summary" />
+        <meta property="twitter:site" content="@sako_data" />
+      </Head>
       <Component {...pageProps} />
     </>
   )
