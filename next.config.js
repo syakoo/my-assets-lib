81314bd0b48ef65204b77a7bcb69b3d6908d0364
syakoo <sakoooo1001@gmail.com>
2020-07-26 20:26:04 +0900

    Fix: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/my-assets-lib' : '',
   publicRuntimeConfig: {
     basePath: process.env.NODE_ENV === 'production' ? '/my-assets-lib' : ''
   }
