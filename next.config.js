5dfdc10f03210de0bd35d15de325eedc7b6d2640
syakoo <nichijou4009@gmail.com>
2020-07-26 12:40:14 +0900

    Fix: update next.config.js for a gh-pages route

--- next.config.js
+++ next.config.js
@@ -1,3 +1,6 @@
 module.exports = {
-  assetPrefix: process.env.NODE_ENV === 'production' ? '/my-assets-lib' : ''
+  assetPrefix: process.env.NODE_ENV === 'production' ? '/my-assets-lib' : '',
+  publicRuntimeConfig: {
+    basePath: process.env.NODE_ENV === 'production' ? '/my-assets-lib' : ''
+  }
 }
