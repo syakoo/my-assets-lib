ff7c84112ff369357392ec44c9ab09080dfdd778
syakoo <syakoo@users.noreply.github.com>
2020-07-26 13:12:28 +0000

    deploy: 8d2fe91c2b618930ae1862da40f7ffc3e7724c3f

---
+++ _next/static/s7CSqTtDYtbgA_FLYCbGy/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fcomponents\u002F[id]","\u002Fsvg\u002F[name]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
--- _next/static/zzvOUb7X0c9t18uNu3uWX/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fcomponents\u002F[fileName]","\u002Fsvg\u002F[name]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
