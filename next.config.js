b28b5c1429e6ad685dbb07543913d39ab2f68aac
syakoo <sakoooo1001@gmail.com>
2020-07-26 20:32:57 +0900

    Fix: undo next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 module.exports = {
+  assetPrefix: process.env.NODE_ENV === "production" ? "/my-assets-lib" : "",
   publicRuntimeConfig: {
     basePath: process.env.NODE_ENV === 'production' ? '/my-assets-lib' : ''
   }
